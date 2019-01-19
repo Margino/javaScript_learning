c9ac7941a573629bed2cf38532e420e86b115fb2
Alex Petrov <i@petrov.com.ru>
2019-01-19 18:07:32 +0300

    reverse a string with the for statement

--- JavaScriptAlgorithms/reverseString.js
+++ JavaScriptAlgorithms/reverseString.js
@@ -2,8 +2,16 @@
 //     return str.split('').reverse().join('');
 // };
 
+// const revString = (str) => {
+//     return [...str].reverse().join('');
+// };
+
 const revString = (str) => {
-    return [...str].reverse().join('');
+    let reverseString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseString += str[i];
+    }
+    return reverseString;
 };
 
 console.log(revString('car'));
