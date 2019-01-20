a65bcbd3798cc45d2e32b029bd5aff3f930b7313
Alex Petrov <i@petrov.com.ru>
2019-01-20 15:00:43 +0300

    getters & setters

--- OOPinJS/abstraction.js
+++ OOPinJS/abstraction.js
@@ -1,16 +1,23 @@
 function Circle(radius) {
     this.radius = radius;
-    let defaultLocation = { x: 0, y: 0 };
-    let compliteOptimunLocation = function() {
-        // some code here
-    };
+
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if (!value.x || !value.y) {
+                throw new Error('Invalid location');
+            }
+            defaultLocation = value;
+        }
+    });
+
     this.draw = function() {
-        compliteOptimunLocation(0.1);
-        // defaultLocation
-        // this.radius
         console.log(draw);
     };
 }
 
 const circle = new Circle(10);
+circle.defaultLocation = { x:1, y:1}
 console.log(circle);
