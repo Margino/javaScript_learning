0e55cf93bd0cc69bde79b1ca44cbd64e37e1e5a7
Alex Petrov <i@petrov.com.ru>
2019-01-25 00:33:32 +0300

    exercise 1

---
+++ ES6everyone/lab1.js
@@ -0,0 +1,5 @@
+// use the array filter method and an arrow function to create an array of the numbers greater than `70`
+const numbers = [3, 62, 234, 7, 23, 74, 23, 76, 92];
+
+const GREATER_NUMBER = numbers.filter(number => number > 70);
+console.log(GREATER_NUMBER);
