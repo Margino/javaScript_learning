910b56c2d5e39c20fbbccfc6dbc524dbc28e43eb
Alex Petrov <i@petrov.com.ru>
2019-01-26 16:44:48 +0300

    create a promise

--- ES6everyone/promise.js
+++ ES6everyone/promise.js
@@ -1,14 +1,20 @@
 const fetch = require('node-fetch');
 
 console.log('goint to fetch');
-const postPromise = fetch('https://rawgithubusercontent.com/Margino/nodejs_learning/master/weather/playground/data1111.json')
+const postPromise = fetch('https://raw.githubusercontent.com/Margino/nodejs_learning/master/weather/playground/data1111.json')
 
 postPromise
     .then(data => data.json())
-    .then(data => console.log(data))
+    // .then(data => console.log(data))
     .catch(err => console.log('Err:', err))
-
 console.log('Done!');
 
 
-postPromise;
+const p = new Promise((resolve, reject) => {
+    // resolve(`It's cool!`);
+    reject(`It's not cool!`);
+});
+
+p
+    .then(data => console.log(data))
+    .catch(err => console.log(`Err:`,err))
