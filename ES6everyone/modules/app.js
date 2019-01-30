9562786cc54e7c75c9fe97b21b9132d436265b83
Alex Petrov <i@petrov.com.ru>
2019-01-30 15:01:30 +0300

    modules

---
+++ ES6everyone/modules/app.js
@@ -0,0 +1,5 @@
+// import {unid} from 'lodash';
+import insane from 'insane';
+import jsonp from 'jsonp';
+import { apiKey as key, first, third } from './config';
+console.log(key, first, third);
---
+++ ES6everyone/modules/config.js
@@ -0,0 +1,7 @@
+export const apiKey = 123;
+// export default apiKey;
+
+const first  = 'John';
+const second = 'Smith';
+
+export { first, second as third };
