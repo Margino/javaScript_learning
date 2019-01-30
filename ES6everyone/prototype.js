16ea67433f1bd52fd8e7d997d9b93bcf5029cce2
Alex Petrov <i@petrov.com.ru>
2019-01-30 23:31:06 +0300

    fix a type

--- ES6everyone/prototype.js
+++ ES6everyone/prototype.js
@@ -6,7 +6,7 @@ Dog.prototype.bark = function() {
     console.log(`Bark, bark! My name is ${this.name.toUpperCase()}!`);
 }
 
-const jack = new Dog('Jack','shepard');
+const jack = new Dog('Jack','shepherd');
 const mik = new Dog('Mic', 'poodle');
 jack.bark();
 mik.bark();
